0600a53336bf4c5bae95a108b42e3f3c17278f9d
buranJ <zhunusburan@gmail.com>
2025-11-10 04:21:19 -0700

    Refactor like handling and improve bookmarks display logic

--- js/main.js
+++ js/main.js
@@ -1,138 +1,137 @@
-let Likes = JSON.parse(localStorage.getItem('like')) || []
-let sneakers = JSON.parse(localStorage.getItem('sneakers')) || []
+let likes = JSON.parse(localStorage.getItem('like')) || [];
+let sneakers = JSON.parse(localStorage.getItem('sneakers')) || [];
 
+const content = document.querySelector('.sneakers__content');
+const searchInput = document.querySelector('.sneakers__inp');
+const searchButton = document.querySelector('.sneakers__lupa');
 
+const cardLikeIcon = '../imgs/cardlike.svg';
+const redLikeIcon = '../imgs/RedLike.svg';
 
-
-
-
-const content = document.querySelector('.sneakers__content')
-
-
-const CardLike = '../imgs/cardlike.svg';
-const RedLike = '../imgs/RedLike.svg'
-
-function renderCard(crasowok) {
-    content.innerHTML = " "
-    crasowok.forEach(item => {
+function showAllCards() {
+    content.innerHTML = "";
+    
+    sneakers.forEach((item) => {
         content.innerHTML += `
-                         <div class="purchases__card">
-                    <div data-id="${item.id}" class="purchases__like">
-                        <img class="" src="${item.add ? RedLike : CardLike}" alt="like">
+            <div class="purchases__card">
+                <div data-id="${item.id}" class="purchases__like">
+                    <img src="${item.add ? redLikeIcon : cardLikeIcon}" alt="like">
+                </div>
+                <img src="${item.image}" alt="" class="purchases__crosovok">
+                <p class="purchases__about">${item.title}</p>
+                <div class="purchases__prise">
+                    <p class="purchases__prise-text">
+                        <span>ЦЕНА:</span> ${item.price}
+                    </p> 
+                    <div class="purchases__prise-plus">
+                        <img class="purchases__prise-img" src="../imgs/plus.svg" alt="plus">
                     </div>
-                    <img  src="${item.image}" alt="" class="purchases__crosovok">
-                    <p class="purchases__about">${item.title}</p>
-                    <div class="purchases__prise">
-                        <p class="purchases__prise-text">
-                            <span>ЦЕНА:</span>  ${item.price}
-                        </p> 
-                        <div class="purchases__prise-plus">
-                            <img class="purchases__prise-img" src="../imgs/plus.svg" alt="plus">
-                        </div>
-                    </div>
-                </div> 
-        `
+                </div>
+            </div>
+        `;
     });
-
-    const likes = document.querySelectorAll('.purchases__like')
-    likes.forEach((elemt) => {
-        elemt.addEventListener('click', () => {
-            let id = elemt.getAttribute('data-id')
-            // console.log(id);
-            const newSneak = sneakers.filter((item) => {
-                if (item.id == id) {
-                    return item
-                }
-            }).map((item) => {
-                return {
-                    "id": item.id,
-                    "title": item.title,
-                    "price": item.price,
-                    "add": true,
-                    "image": item.image  // «Pine Green/White» Suede вариант. :contentReference[oaicite:0]{index=0}
-                }
-            })
-
-            sneakers = sneakers.map((item) => {
-                if (item.id == id) {
-                    return {
-                        "id": item.id,
-                        "title": item.title,
-                        "price": item.price,
-                        "add": !item.add,
-                        "image": item.image  // «Pine Green/White» Suede вариант. :contentReference[oaicite:0]{index=0}
-                    }
-
-                }
-                else {
-                    return item
-                }
-            })
-            const found = Likes.find((item) => item.id == id)
-
-
-            if (!found) {
-                Likes.push(newSneak[0])
-                console.log(Likes);
-
-            }
-            else {
-                Likes.splice(Likes.indexOf(found), 1)
-
-
-            }
-            localStorage.setItem("like", JSON.stringify(Likes))
-
-
-            localStorage.setItem("sneakers", JSON.stringify(sneakers))
-            renderCard(sneakers)
-
-        })
-    })
+    
+    addLikeListeners();
 }
 
-renderCard(sneakers)
+function addLikeListeners() {
+    const likeButtons = document.querySelectorAll('.purchases__like');
+    
+    likeButtons.forEach((button) => {
+        button.addEventListener('click', likeButtonClicked);
+    });
+}
 
-const input = document.querySelector('.sneakers__inp')
-const btn = document.querySelector('.sneakers__lupa')
+function likeButtonClicked(event) {
+    const button = event.currentTarget;
+    const id = button.getAttribute('data-id');
+    
+    sneakers = sneakers.map((item) => {
+        if (item.id == id) {
+            item.add = !item.add;
+            
+            if (item.add) {
+                addToLikes(item);
+            } else {
+                removeFromLikes(id);
+            }
+        }
+        return item;
+    });
+    
+    localStorage.setItem('sneakers', JSON.stringify(sneakers));
+    localStorage.setItem('like', JSON.stringify(likes));
+    
+    showAllCards();
+}
 
-let val
+function addToLikes(sneaker) {
+    const alreadyExists = likes.find((item) => item.id == sneaker.id);
+    
+    if (!alreadyExists) {
+        likes.push({
+            id: sneaker.id,
+            title: sneaker.title,
+            price: sneaker.price,
+            add: true,
+            image: sneaker.image
+        });
+    }
+}
 
-btn.addEventListener('click', () => {
+function removeFromLikes(id) {
+    likes = likes.filter((item) => item.id != id);
+}
 
-    val = input.value
-    console.log(val);
-    search
-})
-
-
-function search() {
-
-    const filterSneakers = sneakers.filter((item) => {
-        if (item.price != val) return item
-    })
-    console.log(filterSneakers);
-    filterSneakers.forEach((item) => {
-        content.innerHTML = ""
+function searchProducts() {
+    const searchText = searchInput.value.toLowerCase();
+    
+    if (searchText === '') {
+        showAllCards();
+        return;
+    }
+    
+    const foundProducts = sneakers.filter((item) => {
+        const sneakerTitle = item.title.toLowerCase();
+        return sneakerTitle.includes(searchText);
+    });
+    
+    content.innerHTML = "";
+    
+    if (foundProducts.length === 0) {
+        content.innerHTML = '<p style="font-size: 18px; text-align: center;">Ничего не найдено</p>';
+        return;
+    }
+    
+    foundProducts.forEach((item) => {
         content.innerHTML += `
-                         <div class="purchases__card">
-                    <div data-id="${item.id}" class="purchases__like">
-                        <img class="" src="${item.add ? RedLike : CardLike}" alt="like">
+            <div class="purchases__card">
+                <div data-id="${item.id}" class="purchases__like">
+                    <img src="${item.add ? redLikeIcon : cardLikeIcon}" alt="like">
+                </div>
+                <img src="${item.image}" alt="" class="purchases__crosovok">
+                <p class="purchases__about">${item.title}</p>
+                <div class="purchases__prise">
+                    <p class="purchases__prise-text">
+                        <span>ЦЕНА:</span> ${item.price}
+                    </p> 
+                    <div class="purchases__prise-plus">
+                        <img class="purchases__prise-img" src="../imgs/plus.svg" alt="plus">
                     </div>
-                    <img  src="${item.image}" alt="" class="purchases__crosovok">
-                    <p class="purchases__about">${item.title}</p>
-                    <div class="purchases__prise">
-                        <p class="purchases__prise-text">
-                            <span>ЦЕНА:</span>  ${item.price}
-                        </p> 
-                        <div class="purchases__prise-plus">
-                            <img class="purchases__prise-img" src="../imgs/plus.svg" alt="plus">
-                        </div>
-                    </div>
-                </div> 
-        `
-    })
-
+                </div>
+            </div>
+        `;
+    });
+    
+    addLikeListeners();
 }
 
+searchButton.addEventListener('click', searchProducts);
 
+searchInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        searchProducts();
+    }
+});
+
+showAllCards();

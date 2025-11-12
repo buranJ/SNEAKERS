232c202848269959534c7b13030ca87a182a58f9
buranJ <zhunusburan@gmail.com>
2025-11-12 05:59:22 -0700

    add "cart"

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const content = document.querySelector('.sneakers__content');
 const searchInput = document.querySelector('.sneakers__inp');
 const searchButton = document.querySelector('.sneakers__lupa');
 
+
 const cardLikeIcon = '../imgs/cardlike.svg';
 const redLikeIcon = '../imgs/RedLike.svg';
 
@@ -24,7 +25,7 @@ function showAllCards() {
                     <p class="purchases__prise-text">
                         <span>ЦЕНА:</span> ${item.price}
                     </p> 
-                    <div class="purchases__prise-plus">
+                    <div data-cart="${item.id}" class="purchases__prise-plus">
                         <img class="purchases__prise-img" src="../imgs/plus.svg" alt="plus">
                     </div>
                 </div>
@@ -33,6 +34,7 @@ function showAllCards() {
     });
 
     addLikeListeners();
+    funCart()
 }
 
 function addLikeListeners() {
@@ -145,6 +147,7 @@ showAllCards();
 const cart = document.querySelector('.header__icon')
 const allCart = document.querySelector('.cart__content')
 const x = document.querySelector('.cart__x')
+const cartSneak=document.querySelector('.cart__sneakers')
 
 
 cart.addEventListener('click', () => {
@@ -154,18 +157,51 @@ cart.addEventListener('click', () => {
 x.addEventListener('click', () => {
     allCart.style.right = '-500px'
 })
-const disCart=document.querySelector('.cart__sneakers')
+const disCart=document.querySelector('.cart__content')
 function renderCart(){
     if(Cart.length==0){
-       disCart.innerHTML=`<div class="cart__empty">
+       disCart.innerHTML+=`<div class="cart__empty">
         <img src="./imgs/cart.svg" alt="cart empty" class="cart__empty-img">
         <h2 class="cart__empty-title">Корзина пустая</h2>
         <p class="cart__empty-text">Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.</p>
         <button class="cart__empty-btn">Вернуться назад</button>
     </div>`
     }
+    else{
+      
+        
+        const sneak=Cart.map((item)=>{
+return `<div class="sneakers__content">
+                    <img src="" alt="" class="sneakers__img">
+                    <div class="sneakers__text">
+                        <h2 class="sneakers__title">${item.title}</h2>
+                        <h2 class="sneakers__price"></h2>
+                    </div>
+                    <button class="sneakers__btn"></button>
+                </div>`
+        })
+        console.log(sneak);
+        
+        cartSneak.innerHTML=sneak[0]
+        
+    }
 }
 renderCart()
+
+
+
+function funCart(){
+    const addBtns=document.querySelectorAll('.purchases__prise-plus')
+  addBtns.forEach((add)=>{
+    add.addEventListener('click',()=>{
+      const addID=add.getAttribute('data-cart')
+      const filCart=sneakers.filter((item)=>item.id==addID)
+      Cart.push(filCart[0])
+      localStorage.setItem('cart', JSON.stringify(Cart))
+      
+    })
+  })
+}
 
 
 
